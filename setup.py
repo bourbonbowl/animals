171d5aa2d0d81ef9c93637d0c3ed7395dcdf4710
bourbonbowl <161977555+bourbonbowl@users.noreply.github.com>
2024-06-08 15:02:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+from setuptools import setup
 
+setup(
+   name='animals',
+   version='1.0',
+   description='A useful animals module',
+   author='animals',
+   author_email='animals@animals.example',
+   packages=['animals'],  #same as name
+)
