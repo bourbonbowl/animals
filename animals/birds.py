f3d77d2a4b4df39643432bd3a92a8fa09b4adf5b
bourbonbowl <161977555+bourbonbowl@users.noreply.github.com>
2024-06-08 19:47:59 -0400

    Update birds.py

--- animals/birds.py
+++ animals/birds.py
@@ -1,4 +1,3 @@
-[python]
 class Birds:
 def __init__(self):
 ''' Constructor for this class. '''
@@ -9,4 +8,3 @@ def printMembers(self):
 print('Printing members of the Birds class')
 for member in self.members:
 print('\t%s ' % member)
-[/python]
