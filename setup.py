75400cba3af07a5f055121147847ca2d625d4b31
bourbonbowl <161977555+bourbonbowl@users.noreply.github.com>
2024-06-09 17:25:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='animals',
-   version='2.1.4',
+   version='2.2.1',
    description='A useful animals module',
    author='animals',
    author_email='animals@animals.example',
