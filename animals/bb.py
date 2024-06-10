87e0a3344e6178a8d809dbc1977512910675b282
bourbonbowl <161977555+bourbonbowl@users.noreply.github.com>
2024-06-09 21:23:12 -0400

    Add files via upload

---
+++ animals/bb.py
@@ -0,0 +1,15 @@
+import json
+import requests
+from google.cloud import storage
+import random
+import datetime
+
+
+def fetch_users():
+    print('a')
+
+def fetch_users_2():
+    print('b')
+
+def fetch_users_3():
+    print('c')
